c5810b87771cacbe8b54c7eab56c2da5489c66fb
Diogo Gabardo <thenolance10@gmail.com>
2023-08-05 11:02:26 -0300

    Primeiro commit para o projeto do curso de SpringBoot

---
+++ src/main/java/com/mballem/curso/boot/domain/Departamento.java
@@ -0,0 +1,21 @@
+package com.mballem.curso.boot.domain;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "DEPARTAMENTOS")
+public class Departamento extends AbstractEntity<Long>{
+
+	@Column(name = "nome", nullable = false, unique = true, length = 60)
+	private String nome;
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	
+}
